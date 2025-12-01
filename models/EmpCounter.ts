3a5aef7d739ba9b7cd591a63e5eaaaa8cb860003
Divyanshu Chandil <chandildivyanshu@gmail.com>
2025-12-01 21:39:30 +0530

    forth-commit

--- models/EmpCounter.ts
+++ models/EmpCounter.ts
@@ -1,16 +1,16 @@
-import mongoose, { Schema, Document, Model } from 'mongoose';
+import mongoose, { Schema, Document, Model } from "mongoose";
 
-export interface IEmpCounter extends Document {
-    _id: string; // "RBM", "AreaManager", "TerritoryManager", "MDO"
+export interface IEmpCounter extends Document<string> {
+    _id: string; // use string as _id
     lastNumber: number;
 }
 
-const EmpCounterSchema: Schema = new Schema({
+const EmpCounterSchema: Schema<IEmpCounter> = new Schema({
     _id: { type: String, required: true },
     lastNumber: { type: Number, default: 0 },
 });
 
 const EmpCounter: Model<IEmpCounter> =
-    mongoose.models.EmpCounter || mongoose.model<IEmpCounter>('EmpCounter', EmpCounterSchema);
+    mongoose.models.EmpCounter || mongoose.model<IEmpCounter>("EmpCounter", EmpCounterSchema);
 
 export default EmpCounter;
